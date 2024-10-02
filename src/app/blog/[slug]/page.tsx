d731a3c58b5221b8d704949039d749db175fbb8a
Romeo Noveanre <noveanrer@gmail.com>
2024-10-02 16:25:22 +0700

    fix: remove unnecessary component

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -61,7 +61,34 @@ export default async function SlugBlog({ params: { slug } }: SlugBlogProps) {
 
   return (
     <div className="prose prose-slate prose-lg mx-auto">
-      <NavBlog estimatedReadingTime={estimatedReadingTime} date={date} />
+      <div className="text-mediumGray text-base flex justify-between items-center mb-5">
+        <Link
+          href="/blog"
+          className="flex items-center justify-center space-x-2 text-mediumGray no-underline"
+        >
+          <svg
+            width="8"
+            height="13"
+            viewBox="0 0 8 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.5 1.5L1.5 6.5L6.5 11.5"
+              stroke="#747474"
+              strokeWidth="1.66667"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>{" "}
+          <span>Return to Blog</span>
+        </Link>
+        <div className="space-x-[8px]">
+          <span>{estimatedReadingTime}</span>
+          <span> &#x2022;</span>
+          <span> {date}</span>
+        </div>
+      </div>
       {content}
     </div>
   );
