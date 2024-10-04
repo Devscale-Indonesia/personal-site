87cbb858a35a3c3ebc538d364a7c438c55ecc3d9
arifaeva <arifa.eva.ixa@gmail.com>
2024-10-05 01:01:23 +0700

    add mobile responsiveness in footer component

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="flex justify-center items-center bg-zinc-50">
-      <div className="max-w-[1080px] w-full border border-t border-l-0 border-r-0 border-b-0 px-16 py-8">
+      <div className="max-w-[1080px] w-full border border-t border-l-0 border-r-0 border-b-0 lg:px-16 lg:py-8 py-3">
         <p className="text-center text-gray-500">
           <span className="font-semibold">&copy;Devscale.</span> Build with
         </p>
