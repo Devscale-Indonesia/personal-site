b4cc36702da0ad3d08b153623939f557dfd4822b
arifaeva <arifa.eva.ixa@gmail.com>
2024-10-04 22:56:27 +0700

    change custom color classname into tailwind color classname and layout improvement in blog page

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,8 +2,10 @@ import type { ReactNode } from "react";
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-[1080px] w-full mt-[5rem] p-16 mx-auto">
-      {children}
+    <div className="lg:flex lg:justify-center lg:items-center bg-zinc-50">
+      <div className="max-w-[1080px] w-full mt-[5rem] p-16 bg-zinc-50 min-h-screen">
+        {children}
+      </div>
     </div>
   );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,7 +7,7 @@ export default async function Blog() {
   return (
     <>
       <h1 className="text-4xl mb-3 font-semibold">Blog</h1>
-      <p className="text-mediumGray mb-12">
+      <p className="text-neutral-500 mb-12">
         Stay updated with my insights on industry trends, personal experiences,
         and more.
       </p>
