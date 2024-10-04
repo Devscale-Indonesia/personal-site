03c4f955550753ef9a122f1d45f51b4c604d5679
arifaeva <arifa.eva.ixa@gmail.com>
2024-10-04 22:26:17 +0700

    change custom color className into tailwind color className, add interactivity, remove unnecessary code in blog card component

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,28 +1,20 @@
 import Link from "next/link";
 import React from "react";
-import { twMerge } from "tailwind-merge";
-import { tv, VariantProps } from "tailwind-variants";
-
-const style = tv({
-  base: "border-silver border rounded-xl bg-white p-4 h-24 max-w-[300px] h-[220px] flex flex-col justify-between",
-});
-
-type TCard = VariantProps<typeof style>;
-interface Props extends TCard, React.ComponentPropsWithRef<"div"> {}
 
 interface Props {
   date: string;
   title: string;
   url: string;
+  children: React.ReactNode;
 }
 
 export const BlogCard = (props: Props) => {
   return (
     <Link href={props.url} className="w-fit block mx-auto sm:mx-none">
-      <div {...props} className={twMerge(style({ ...props }))}>
+      <div className="border-zinc-300 border rounded-xl bg-white p-4 max-w-[300px] h-[220px] flex flex-col justify-between hover:drop-shadow-md active:drop-shadow-md transition-all duration-300">
         <div className="space-y-3">
-          <span className="text-sm text-mediumGray">{props.date}</span>
-          <h2 className="text-blackColor font-semibold line-clamp-2 text-lg">
+          <span className="text-sm text-neutral-500">{props.date}</span>
+          <h2 className="text-black font-semibold line-clamp-2 text-lg">
             {props.title}
           </h2>
         </div>
