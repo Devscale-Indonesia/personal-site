571d41b98e7b16ccf2868876ebf756792de286d1
arifaeva <arifa.eva.ixa@gmail.com>
2024-10-04 21:56:50 +0700

    change color className into tailwind color className, add animation, remove redundant className in experience card component

--- src/components/experience-card.tsx
+++ src/components/experience-card.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const PresentTag = () => {
   return (
-    <span className="font-normal text-white uppercase text-sm bg-blackColor rounded-full px-5 flex items-center justify-center">
+    <span className="font-normal text-white uppercase text-sm bg-black rounded-full px-5 flex items-center justify-center">
       Present
     </span>
   );
@@ -19,42 +19,32 @@ export const PresentTag = () => {
 
 export const ExperienceCard = (props: Props) => {
   return (
-    <div className="h-full w-full relative flex">
-      <div>
-        <div className=" bg-[#DEDEDE] w-[1px] h-full"></div>
-        {props.endPeriod ? (
-          <div className="absolute w-3 h-3 rounded-full z-100 bg-[#DEDEDE] -left-1.5 top-0"></div>
-        ) : (
-          <div className="absolute w-3 h-3 rounded-full z-100 bg-blackColor -left-1.5 top-0"></div>
-        )}
-      </div>
-
-      <div className="h-full w-full relative pb-3">
+    <div className="h-full w-full border border-l border-t-0 border-b-0 border-r-0 border-zinc-300 group">
+      <div className="relative pb-3">
         <input
           type="checkbox"
-          className="peer absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer"
+          className="peer absolute top-0 inset-x-0 w-full h-20 opacity-0 z-10 cursor-pointer"
         />
-        <div className="space-y-3 pl-8 py-3">
-          <div className="flex justify-between items-center">
-            <div className="flex flex-col">
-              <h4 className="flex gap-4 font-semibold">
-                {props.company}
-                {props.endPeriod ? null : <PresentTag />}
-              </h4>
-              <p className="text-[#979797]">{props.position}</p>
-            </div>
-            <p className="text-[#979797] uppercase">
-              {props.startPeriod}
-              {props.endPeriod ? ` - ${props.endPeriod}` : null}
-            </p>
+        <div className="absolute w-3 h-3 rounded-full z-100 bg-zinc-300 -left-1.5 top-0 group-hover:bg-black peer-checked:bg-black"></div>
+        <div className="flex justify-between items-center space-y-3 pl-8 py-3">
+          <div className="space-y-1">
+            <h4 className="flex gap-4 font-semibold">
+              {props.company}
+              {props.endPeriod ? null : <PresentTag />}
+            </h4>
+            <p className="text-neutral-400">{props.position}</p>
           </div>
+          <p className="text-neutral-400 uppercase">
+            {props.startPeriod}
+            {props.endPeriod ? ` - ${props.endPeriod}` : null}
+          </p>
         </div>
 
-        <div className="space-y-1 overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-none pl-8">
-          <p className="text-mediumGray">{props.description}</p>
-          <p className="text-mediumGray">Key Responsibilites:</p>
+        <div className="space-y-1 overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-[500px] pl-8">
+          <p className="text-neutral-500">{props.description}</p>
+          <p className="text-neutral-500">Key Responsibilites:</p>
           {props.keys?.map((key, index) => (
-            <p key={index} className="text-mediumGray">
+            <p key={index} className="text-neutral-500">
               &#8226; {key}
             </p>
           ))}
