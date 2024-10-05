c18039c64cc29406ad912f535a501818dd646fe1
arifaeva <arifa.eva.ixa@gmail.com>
2024-10-06 00:16:54 +0700

    dark mode in blog-card component (just color placeholder, not fixed design)

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -11,10 +11,12 @@ interface Props {
 export const BlogCard = (props: Props) => {
   return (
     <Link href={props.url} className="w-fit block mx-auto sm:mx-none">
-      <div className="border-zinc-300 border rounded-xl bg-white p-4 max-w-[300px] h-[220px] flex flex-col justify-between hover:drop-shadow-md active:drop-shadow-md transition-all duration-300">
+      <div className="border-zinc-300 dark:hover:border-zinc-200 dark:border-0 dark:hover:shadow-sm dark:hover:shadow-zinc-400 border rounded-xl bg-white dark:bg-zinc-700 p-4 max-w-[300px] h-[220px] flex flex-col justify-between hover:drop-shadow-md active:drop-shadow-md transition-all duration-300">
         <div className="space-y-3">
-          <span className="text-sm text-neutral-500">{props.date}</span>
-          <h2 className="text-black font-semibold line-clamp-2 text-lg">
+          <span className="text-sm text-neutral-500 dark:text-neutral-400">
+            {props.date}
+          </span>
+          <h2 className="text-black dark:text-white font-semibold line-clamp-2 text-lg">
             {props.title}
           </h2>
         </div>
